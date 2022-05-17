6dbdeda27cf3f9b44d7228653ffdedfbaa891429
Lorenzo Racca <269760@studenti.unimore.it>
2022-05-17 14:28:16 +0200

    🐛 Not logged redirect fix

--- moviesreviews/moviesreviews/settings.py
+++ moviesreviews/moviesreviews/settings.py
@@ -127,4 +127,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/?login=ok'
+LOGIN_URL = "/login/?auth=notok"
