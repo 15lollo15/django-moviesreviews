991f479a066d8d6c30135b29a3fb90e0e9c8965c
Lorenzo <lorenzo_racca@hotmail.com>
2022-05-21 11:59:39 +0200

    🖌 Ugly director selector

--- moviesreviews/movies/views.py
+++ moviesreviews/movies/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
-from movies.models import Genre, Movie
+from movies.models import Genre, Movie, Person
 from users.models import Review
 from users.views import ReviewForm
 
@@ -36,16 +36,31 @@ class SearchView(ListView):
         title = self.request.GET.get('title', None)
         if title != None:
             movies = movies.filter(title__icontains = title)
+        
         genre_r = self.request.GET.get('genre', None)
         if genre_r != None and genre_r != "":
-            genre = Genre.objects.filter(name=genre_r)
+            genre = Genre.objects.filter(pk=genre_r)
             movies = movies.filter(genre__in= genre)
+
+        directors_r = self.request.GET.get('director', None)
+        if directors_r != None and directors_r != "":
+            directors = Person.objects.filter(pk=directors_r)
+            movies = movies.filter(directors__in= directors)
+
         return movies
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["genres"] = Genre.objects.all()
-        context["selected_genre"] = self.request.GET.get('genre', '')
+        context["directors"] = Person.objects.all()
+        genre = self.request.GET.get('genre', 0)
+        if genre == "":
+            genre = 0
+        context["selected_genre"] = int(genre)
+        director = self.request.GET.get('director', 0)
+        if director == "":
+            director = 0
+        context["selected_director"] = int(director)
         context["title"] = self.request.GET.get('title', "")
         return context
     
